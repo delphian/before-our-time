dd353b0b1805ec2b5c7d59c621c064ac58f14878
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-08-02 21:52:49 +0000

    Updating scripts

--- start-servers.sh
+++ start-servers.sh
@@ -1,11 +1,7 @@
 cd before-our-time-web-api
-dotnet clean
-dotnet build
 screen -dmS bot_webapi dotnet run
 cd ..
 cd before-our-time-business
-dotnet clean
-dotnet build
 screen -dmS bot_server dotnet run
 cd ..
 
