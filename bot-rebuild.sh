0f238b56d4c65bde39ac05461e2d51f56429f1ad
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-09-02 12:59:05 -0700

    Updating submodules

--- bot-rebuild.sh
+++ bot-rebuild.sh
@@ -1,12 +1,37 @@
 cd before-our-time-models
+rm -Rf obj
+rm -Rf bin
 dotnet clean
 dotnet build
 cd ..
 cd before-our-time-repository
+rm -Rf obj
+rm -Rf bin
 dotnet clean
 dotnet build
 cd ..
 cd before-our-time-business
+rm -Rf obj
+rm -Rf bin
 dotnet clean
 dotnet build
 cd ..
+cd before-our-time-client/BeforeOurTime.MobileApp.Android
+rm -Rf obj
+rm -Rf bin
+dotnet clean
+dotnet build
+cd ..
+cd before-our-time-client/BeforeOurTime.MobileApp.UWP
+rm -Rf obj
+rm -Rf bin
+dotnet clean
+dotnet build
+cd ..
+cd before-our-time-client/BeforeOurTime.MobileApp.iOS
+rm -Rf obj
+rm -Rf bin
+dotnet clean
+dotnet build
+cd ..
+
