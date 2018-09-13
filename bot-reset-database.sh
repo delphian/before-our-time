87cb96905e41b6f4c2551e172f7b2724690f5287
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-09-12 23:24:04 -0700

    Migrating to module system

--- bot-reset-database.sh
+++ bot-reset-database.sh
@@ -2,4 +2,7 @@ cd before-our-time-repository
 echo y|dotnet ef database drop --context MSSQLContext
 dotnet ef database update --context MSSQLContext
 cd ..
+cd before-our-time-business
+dotnet ef database update --context EFGameModuleContext
+cd ..
 
