95f133ea82675bb06beeb83add5d96aceb9fc098
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-12-31 19:46:27 -0800

    Build scripts for gossip portion

--- bot-rebuild.sh
+++ bot-rebuild.sh
@@ -4,28 +4,16 @@ rm -Rf bin
 dotnet clean
 dotnet build
 cd ..
+cd gossip-mud-chat-network
+rm -Rf obj
+rm -Rf bin
+dotnet clean
+dotnet build
+cd ..
 cd before-our-time-business
 rm -Rf obj
 rm -Rf bin
 dotnet clean
 dotnet build
 cd ..
-cd before-our-time-client/BeforeOurTime.MobileApp.Android
-rm -Rf obj
-rm -Rf bin
-dotnet clean
-dotnet build
-cd ../..
-cd before-our-time-client/BeforeOurTime.MobileApp.UWP
-rm -Rf obj
-rm -Rf bin
-dotnet clean
-dotnet build
-cd ../..
-cd before-our-time-client/BeforeOurTime.MobileApp.iOS
-rm -Rf obj
-rm -Rf bin
-dotnet clean
-dotnet build
-cd ../..
 
