37c7478220d4b67819eb9838518a6670ba255e3b
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2019-01-04 15:25:46 -0800

    Updating submodules

--- bot-reset-database.sh
+++ bot-reset-database.sh
@@ -3,4 +3,5 @@ echo y|dotnet ef database drop --context EFCoreModuleContext
 dotnet ef database update --context EFCoreModuleContext
 dotnet ef database update --context EFAccountModuleContext
 dotnet ef database update --context EFWorldModuleContext
+dotnet ef database update --context EFScriptModuleContext
 cd ..
